4b7e15cd7a21a9433ea10c6dfcab39e445565db1
Tharaniesh <94771662+Tharaniesh@users.noreply.github.com>
2026-02-26 19:58:29 +0530

    Change base path in Vite configuration
    Fix vite.config.js

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react-swc';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/PortFolio/',
+  base: '/',
   build: {
     sourcemap: false,
     target: 'es2020'
